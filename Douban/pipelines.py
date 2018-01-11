440593a104b95e6b70e703ffc6e21ecb0dd696e6
hongwang <songhw@tangdou.com>
2018-01-11 11:07:57 +0800

    add timer but need more building

--- Douban/pipelines.py
+++ Douban/pipelines.py
@@ -5,30 +5,33 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import sys
-import json
-import os
+import sys, os, json
+from Douban.test import Test
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
+base_dir = sys.path[0]
 
 class DoubanPipeline(object):
     
     def __init__(self):
-        self.file = open("spiders/list_json.txt", "w")
+        test = Test()
+        file_path =  os.path.split(os.path.realpath(__file__))[0] + "/spiders/list_json.txt"
+        self.file = open(file_path, "w")
         '''开始中括号'''
-        self.file.write("[")
+        self.file.write("[" + "\n")
 
     def process_item(self, item, spider):
 
         #print("item ==== %s"%(item['name']))
         itemJson = json.dumps(dict(item), ensure_ascii = False)
-        self.file.write(itemJson + ",")
+        self.file.write(itemJson + "," + "\n")
         return item
 
 
     def close_spider(self, spider):
         '''结束中括号'''
-        self.file.seek(0, 2)
-        self.file.write("]")
+        self.file.seek(-2, 2)
+        self.file.write("\n" + "]")
 
         self.file.close()
